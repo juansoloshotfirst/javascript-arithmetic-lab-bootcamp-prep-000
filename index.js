d344f3513d7566b3efafcde4891f838a9efe637f
Jon Marquez <jon.marquez9258@gmail.com>
2018-04-07 15:03:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function add (x,y) {
+  return x + y;
+}
+add(1,80);
+function subtract (x,y) {
+  return x - y;
+}
+subtract (60,40);
+function multiply (x,y) {
+  return x * y;
+}
+multiply (2,3.4)
+function divide (x,y) {
+return x / y;
+}
+divide (5.0,2.5)
